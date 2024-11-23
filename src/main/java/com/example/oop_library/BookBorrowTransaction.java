dbf1cd6df792d6a90b96b5374b88900df6291c62
hoang0205 <doanhoang670@gmail.com>
2024-11-23 22:44:25 +0700

    fix some bug

--- src/main/java/com/example/oop_library/BookBorrowTransaction.java
+++ src/main/java/com/example/oop_library/BookBorrowTransaction.java
@@ -13,13 +13,13 @@ public class BookBorrowTransaction {
         //     System.out.println("Please settle the fine to continue using the service!");
         if (user.getBorrowedBooks().size() == 5) {
             return "You can only borrow up to 5 books!\n Return a book before borrowing more.";
-        } else if (books.isBorrowed()) {
+        } else if (b.isBorrowed()) {
             return "This book is already borrowed by another user!";
         } else {     
-            if (books.isBorrowed() == false) {
+            if (b.isBorrowed() == false) {
                 // Cập nhật trạng thái sách
                 BookFormController bookFormController = new BookFormController();
-                bookFormController.CreateUpdateBooksFormStage(books);
+                bookFormController.CreateUpdateBooksFormStage(b);
                 System.out.println(SessionManager.getInstance().getCurrentUser().getId());
                 bookFormController.getBookFormController().getBorrowerIDField().setText(Integer.toString(SessionManager.getInstance().getCurrentUser().getId()));
                 LocalDate borrowDate = LocalDate.now();
@@ -29,10 +29,10 @@ public class BookBorrowTransaction {
                 bookFormController.getBookFormController().Edit();
             }
         }
-        if (user.getBorrowedBooks().indexOf(books) >= 0 && books.isBorrowed() == true) {
+        if (user.getBorrowedBooks().indexOf(b) >= 0 && b.isBorrowed() == true) {
             System.out.println("Completed!");
         } 
-        return String.format("Success! Please remember to return this book by %s", books.getReturnDate());
+        return String.format("Success! Please remember to return this book by %s", b.getReturnDate());
     }
 
     public ImageView getBookImage() {
